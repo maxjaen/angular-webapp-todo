23f8f0c1aa8af394066267425c8b13af15c43c07
Max Jänicke <max.jaenicke@gmx.de>
2020-01-11 15:44:19 +0100

    remove dialog to remove tasks with specified id

--- src/app/standard-layout/tasks/dialogs/remove-task-dialog.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RemoveTaskDialog } from './remove-task-dialog';
-
-describe('RemoveTaskDialog', () => {
-  it('should create an instance', () => {
-    expect(new RemoveTaskDialog()).toBeTruthy();
-  });
-});

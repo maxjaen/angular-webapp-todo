6a064490f0161fb5074a9f10a72e9163355d6059
Max Jänicke <max.jaenicke@gmx.de>
2020-01-11 15:48:04 +0100

    Format with prettier

--- src/app/standard-layout/weight/weight.component.ts
+++ src/app/standard-layout/weight/weight.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Weight } from './model/weight';
-import { WeightService } from './services/weight.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material';
+import { Component, OnInit } from "@angular/core";
+import { Weight } from "./model/weight";
+import { WeightService } from "./services/weight.service";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
-  selector: 'app-weight',
-  templateUrl: './weight.component.html',
-  styleUrls: ['./weight.component.scss']
+  selector: "app-weight",
+  templateUrl: "./weight.component.html",
+  styleUrls: ["./weight.component.scss"]
 })
 export class WeightComponent implements OnInit {
   weights: Weight[];
 
   form = new FormGroup({
-    weight: new FormControl('', [
-      Validators.required
-    ])
+    weight: new FormControl("", [Validators.required])
   });
 
-  constructor(private weightService: WeightService,  private _snackBar: MatSnackBar) {  }
+  constructor(
+    private weightService: WeightService,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
     this.getWeightsFromService();
@@ -28,7 +29,7 @@ export class WeightComponent implements OnInit {
     this.weights = [];
 
     this.weightService.getAllWeights().subscribe(e => {
-      this.weights = e.sort(function (a, b) {
+      this.weights = e.sort(function(a, b) {
         return Date.parse(b.date.toString()) - Date.parse(a.date.toString());
       });
     });
@@ -37,7 +38,7 @@ export class WeightComponent implements OnInit {
   saveWeight() {
     if (this.form.getRawValue().weight != "") {
       let weightValue: number = this.form.getRawValue().weight;
-      let tempWeight: Weight = { id: 0, value: weightValue, date: new Date() }
+      let tempWeight: Weight = { id: 0, value: weightValue, date: new Date() };
 
       if (this.isNumber(tempWeight.value)) {
         this.weightService.postWeight(tempWeight).subscribe(() => {
@@ -45,10 +46,16 @@ export class WeightComponent implements OnInit {
           this.getWeightsFromService();
         });
       } else {
-        console.warn("saveWeight(): ID: " + tempWeight.value + ", expected number")
+        console.warn(
+          "saveWeight(): ID: " + tempWeight.value + ", expected number"
+        );
       }
     } else {
-      console.warn("saveWeight(): Value: " + this.form.getRawValue().weight + ", expected Value")
+      console.warn(
+        "saveWeight(): Value: " +
+          this.form.getRawValue().weight +
+          ", expected Value"
+      );
     }
   }
 
@@ -60,58 +67,61 @@ export class WeightComponent implements OnInit {
           this.getWeightsFromService();
         });
       } else {
-        console.warn("removeTask(): ID: " + weight.id + ", expected number")
+        console.warn("removeTask(): ID: " + weight.id + ", expected number");
       }
     } else {
-      console.warn("removeTask(): ID: " + weight.id + ", expected ID")
+      console.warn("removeTask(): ID: " + weight.id + ", expected ID");
     }
   }
 
   /*
    *
    * HELPER FUNCTIONS
-   * 
+   *
    */
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
-      duration: 2000,
+      duration: 2000
     });
   }
 
   isNumber(param: any): boolean {
-    return !(isNaN(Number(param)))
+    return !isNaN(Number(param));
   }
 
   /*
    *
    * STATISTICS
-   * 
+   *
    */
 
-  getLatestWeight(): Weight{
-    let id: number = this.weights.map(e => e.id).sort(function (a, b) {
-      return a - b;
-    })[this.weights.length - 1];
+  getLatestWeight(): Weight {
+    let id: number = this.weights
+      .map(e => e.id)
+      .sort(function(a, b) {
+        return a - b;
+      })[this.weights.length - 1];
     return this.weights.filter(e => e.id == id)[0];
   }
 
-  getDaysSinceLastWeight(): number{
-    let weight: Weight = this.getLatestWeight(); 
-    
-    if (weight !== undefined){
+  getDaysSinceLastWeight(): number {
+    let weight: Weight = this.getLatestWeight();
+
+    if (weight !== undefined) {
       let tempDate: Date = new Date();
-      let milliseconds: number = tempDate.getTime() - new Date(weight.date).getTime();
-    
-      return Math.floor(((milliseconds) / (1000 * 60 * 60 * 24)));
+      let milliseconds: number =
+        tempDate.getTime() - new Date(weight.date).getTime();
+
+      return Math.floor(milliseconds / (1000 * 60 * 60 * 24));
     }
-     
+
     return -1;
   }
 
-  calculateBMI(){
-    if (this.weights.length != 0){
-      return (+this.getLatestWeight().value / ((1.85)*(1.85))).toFixed(2);
+  calculateBMI() {
+    if (this.weights.length != 0) {
+      return (+this.getLatestWeight().value / (1.85 * 1.85)).toFixed(2);
     }
 
     return -1;
@@ -122,7 +132,10 @@ export class WeightComponent implements OnInit {
   }
 
   getAverageWeightValue(): number {
-    return this.weights.map(e => +e.value).reduce((a, b) => a + b, 0)/this.weights.length;
+    return (
+      this.weights.map(e => +e.value).reduce((a, b) => a + b, 0) /
+      this.weights.length
+    );
   }
 
   getHighestWeightValue(): number {
@@ -130,32 +143,27 @@ export class WeightComponent implements OnInit {
   }
 
   getBackgroundColorValue(weight: Weight): string {
-
-    // if (weight.id == this.weights.length) {
-    //   return '#607D8B';
-    // }
-
     let highestValue: number = this.getHighestWeightValue();
     if (weight.value == highestValue) {
-      return '#758B60';
+      return "#758B60";
     }
 
     let lowestValue: number = this.getLowestWeightValue();
     if (weight.value == lowestValue) {
-      return '#8B6066';
+      return "#8B6066";
     }
 
     let averageValue: number = this.getAverageWeightValue();
-    if ((weight.value <= (averageValue + 1)) && (weight.value >= (averageValue - 1))) {     
-      return '#8B7B60';
+    if (weight.value <= averageValue + 1 && weight.value >= averageValue - 1) {
+      return "#8B7B60";
     }
-    if (weight.value < (averageValue - 1)) {     
-      return '#8B6B60';
+    if (weight.value < averageValue - 1) {
+      return "#8B6B60";
     }
-    if (weight.value > (averageValue + 1)) {     
-      return '#8B8B60';
+    if (weight.value > averageValue + 1) {
+      return "#8B8B60";
     }
 
-    return '424242';
+    return "424242";
   }
 }

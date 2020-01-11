623aec7b034335c0c003d3b3c7093d6bb5b58ce8
Max Jänicke <max.jaenicke@gmx.de>
2020-01-11 15:48:52 +0100

    Format with prettier

--- src/app/standard-layout/training/components/training-view/training-view.component.ts
+++ src/app/standard-layout/training/components/training-view/training-view.component.ts
@@ -1,46 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { TrainingService } from '../../services/training.service';
-import { Training } from '../../model/training';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Exercise } from '../../model/exercise';
+import { Component, OnInit } from "@angular/core";
+import { TrainingService } from "../../services/training.service";
+import { Training } from "../../model/training";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Exercise } from "../../model/exercise";
 
 @Component({
-  selector: 'app-training-view',
-  templateUrl: './training-view.component.html',
-  styleUrls: ['./training-view.component.scss']
+  selector: "app-training-view",
+  templateUrl: "./training-view.component.html",
+  styleUrls: ["./training-view.component.scss"]
 })
 export class TrainingViewComponent implements OnInit {
-
-  actualTraining: Training = {id:0, date: null, description:"", exercices: null};
-  dataSource: Exercise[] = []
-  displayedColumns: string[] = ['name', 'category', 'string'];
-  constructor(private trainingService: TrainingService,  private route: ActivatedRoute, private router: Router) { }
+  actualTraining: Training = {
+    id: 0,
+    date: null,
+    description: "",
+    exercices: null
+  };
+  dataSource: Exercise[] = [];
+  displayedColumns: string[] = ["name", "category", "string"];
+  constructor(
+    private trainingService: TrainingService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.getTrainingFromService();
-   }
+  }
 
-  getTrainingFromService(){
+  getTrainingFromService() {
     this.route.params.subscribe(params => {
-      this.trainingService.getTrainingByID(+params['id'])
-      .subscribe(training => {
-        this.actualTraining = training;  
-        this.dataSource = this.actualTraining.exercices;
-        console.log(this.dataSource);        
-      });
+      this.trainingService
+        .getTrainingByID(+params["id"])
+        .subscribe(training => {
+          this.actualTraining = training;
+          this.dataSource = this.actualTraining.exercices;
+          console.log(this.dataSource);
+        });
     });
   }
 
-  removeTraining(){
-    this.route.params.subscribe(params => {
-      this.trainingService.deleteTrainingByID(+params['id'])
-      .subscribe(() => {
-          this.gotoTraining();  
-      })
-    });
+  removeTraining() {
+    if (window.confirm("Are sure you want to delete this item ?")) {
+      this.route.params.subscribe(params => {
+        this.trainingService.deleteTrainingByID(+params["id"]).subscribe(() => {
+          this.gotoTraining();
+        });
+      });
+    }
   }
 
   gotoTraining() {
-    this.router.navigate(['/training']);
+    this.router.navigate(["/training"]);
   }
 }

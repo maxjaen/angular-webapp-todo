7f893e6903a4235e65ba230a1d09644b0c052cbd
Max Jänicke <max.jaenicke@gmx.de>
2020-10-26 21:48:04 +0100

    add daylight saving time switch

--- src/app/standard-layout/shared/services/utils/time.service.ts
+++ src/app/standard-layout/shared/services/utils/time.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { UtilityService } from './utility.service';
 
-const GER_UTC_PLUS_ONE = 1; // TODO maybe there is a way to determine if summertime
+const GER_UTC_PLUS_ONE = 1;
 const GER_UTC_PLUS_TWO = 2;
 
 @Injectable({
@@ -12,7 +12,7 @@ export class TimeService {
 
   public createNewDate(): Date {
     const date: Date = new Date();
-    date.setHours(date.getUTCHours() + GER_UTC_PLUS_TWO);
+    date.setHours(this.isDST(date) ? date.getUTCHours() + GER_UTC_PLUS_TWO: date.getUTCHours() + GER_UTC_PLUS_ONE);
 
     return date;
   }
@@ -91,5 +91,20 @@ export class TimeService {
       unknownDate.getMonth() === currentDate.getMonth() &&
       unknownDate.getFullYear() === currentDate.getFullYear()
     );
+  }
+
+  /**
+   * Checks if a date is in the Daylight Saving Time (DST) in germany.
+   * DST starts on Sunday 29 March 2020, 02:00
+   * DST ends on Sunday 25 October 2020, 03:00 
+   * https://24timezones.com/Germany/time
+   * https://stackoverflow.com/questions/11887934/how-to-check-if-dst-daylight-saving-time-is-in-effect-and-if-so-the-offset
+   * 
+   * @param date to be checked if in dst or not
+   */
+  private isDST(date: Date) {
+    let jan = new Date(date.getFullYear(), 0, 1).getTimezoneOffset();
+    let jul = new Date(date.getFullYear(), 6, 1).getTimezoneOffset();
+    return Math.max(jan, jul) != date.getTimezoneOffset(); 
   }
 }
